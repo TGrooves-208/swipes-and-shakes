5c4accf4854ed25554ad26b2c936427c2ddac516
TGrooves-208 <tgrooves.208@gmail.com>
2016-01-28 22:16:54 -0700

    added basic comments

--- swipes-and-shakes/ViewController.swift
+++ swipes-and-shakes/ViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  swipes-and-shakes
-//
+//  A basic showing of how to work with simple gestures
 //  Created by Gil Aguilar on 1/28/16.
 //  Copyright © 2016 redeye-dev. All rights reserved.
 //
@@ -12,8 +12,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        var swipeUp = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view.addGestureRecognizer(swipeUp)
+        
     }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        
+        if event?.subtype == UIEventSubtype.MotionShake {
+            print("Device was shaken not stirred")
+            
+        }
+    }
+    
+    func swiped(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipeGesture.direction {
+                
+            case UISwipeGestureRecognizerDirection.Right:
+                print("Swiped Right")
+            case UISwipeGestureRecognizerDirection.Up:
+                print("Swiped Up")
+            default:
+                break
+                
+            }
+            
+            
+            
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
