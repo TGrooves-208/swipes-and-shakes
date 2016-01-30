64d854fb7879a809a9359cab23fbf0b613d3c59a
TGrooves-208 <tgrooves.208@gmail.com>
2016-01-30 13:56:46 -0700

    added shake gestures with random sounds

--- swipes-and-shakes/ViewController.swift
+++ swipes-and-shakes/ViewController.swift
@@ -7,49 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
+    var sounds = ["coffeemug1", "coffeemug2", "coffeetable", "instantcoffee1", "instantcoffee2", "pouringcoffee", "teaspoon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var swipeRight = UISwipeGestureRecognizer(target: self, action: "swiped:")
-        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
-        self.view.addGestureRecognizer(swipeRight)
-        
-        var swipeUp = UISwipeGestureRecognizer(target: self, action: "swiped:")
-        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
-        self.view.addGestureRecognizer(swipeUp)
         
     }
     
     override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
         
         if event?.subtype == UIEventSubtype.MotionShake {
-            print("Device was shaken not stirred")
+            
+            var randomNumber = Int(arc4random_uniform(UInt32(sounds.count)))
+            
+            
+            var fileLocation = NSBundle.mainBundle().pathForResource(sounds[randomNumber], ofType: "mp3")
+            
+            do { try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: fileLocation!))
+            player.play()
+                
+            } catch {}
+            
             
         }
     }
     
-    func swiped(gesture: UIGestureRecognizer) {
-        
-        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-            
-            switch swipeGesture.direction {
-                
-            case UISwipeGestureRecognizerDirection.Right:
-                print("Swiped Right")
-            case UISwipeGestureRecognizerDirection.Up:
-                print("Swiped Up")
-            default:
-                break
-                
-            }
-            
-            
-            
-        }
-    }
     
 
     override func didReceiveMemoryWarning() {
